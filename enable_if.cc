d9c3985052d8cc9a72f00ab642dad8e493d9d3e7
yixinglu <yxlyee@gmail.com>
2016-06-18 23:08:05 +0800

    test std::enable_if metafunction

---
+++ enable_if.cc
@@ -0,0 +1,72 @@
+#include <type_traits>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, bool>::type
+is_odd(T i) {
+  return bool(i%2);
+}
+
+template <typename T,
+	  typename = typename std::enable_if<std::is_integral<T>::value>::type
+	>
+bool is_even(T i) {
+	return !is_odd(i);
+}
+
+// -----------------
+template<typename T>
+using type = typename std::enable_if<std::is_integral<T>::value>::type;
+
+// -----------------
+template <bool, typename R=void> 
+struct Test {};
+
+template<typename R>
+struct Test<true, R> {
+ using type = R;
+};
+
+template <typename T>
+typename Test<std::is_integral<T>::value, void>::type
+test(T i) {
+  cout << i << endl;
+  // return i;
+}
+
+// -----------------
+
+template <typename C, typename Enable=void>
+struct run_check { };
+
+template <typename C>
+struct run_check<C,
+		 typename std::enable_if<std::is_integral<C>::value>::type>
+{
+  using type = C;
+};
+
+template <typename C>
+typename run_check<C>::type test_run_check(C c) {
+  cout << c << endl;
+  return c;
+}
+// -----------------
+int main() {
+
+  cout << "5 is odd:" << is_odd(5) << endl;
+  // cout << "5.0 is odd:" << is_odd(5.0) << endl;
+  cout << "5 is even:" << is_even(5) << endl;
+  // cout << "5.0 is even:" << is_even(5.0) << endl;
+
+  Test<true, int>::type i;
+  // Test<false, int>::type i;
+  test(5);
+
+  test_run_check(5);
+  // test_run_check(5.0);
+
+  return 0;
+}

da5b4efb97d20081db8e380f98d61592edbe9819
yixinglu <yxlyee@gmail.com>
2016-06-19 09:23:31 +0800

    test and or not logical operator keyword

---
+++ logic_words.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void test_and(bool a, bool b) {
+  if (a and b) {
+    cout << "a && b = " << (a&&b) << endl;
+  }
+}
+
+void test_or(bool a, bool b) {
+  if (a or b) {
+    cout << "a || b = " << (a||b) << endl;
+  }
+}
+
+void test_not(bool b) {
+  if (not b) {
+    cout << "!b = " << (!b) << endl;
+  }
+}
+
+int main() {
+  test_and(true, true);
+  test_and(false, true);
+  test_or(true, false);
+  test_or(false, false);
+  test_not(true);
+  test_not(false); 
+  return 0;
+}

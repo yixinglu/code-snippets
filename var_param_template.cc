bd6026d2c10589d8de2e5c27a791dafef850f036
yixinglu <yxlyee@gmail.com>
2016-06-29 19:44:47 +0800

    add variadic template function

--- var_param_template.cc
+++ var_param_template.cc
@@ -9,8 +9,15 @@ struct logic_helper;
 template <bool ...b>
 using all_t = std::is_same<logic_helper<b...>, logic_helper<(b or true)...>>;
 
+template <bool ...b>
+auto test() -> all_t<b...> {
+  return {};
+}
+
 int main() {
   cout << "all_t<true, true>:" << all_t<true, true>::value << endl;
   cout << "all_t<false, true>:" << all_t<false, true>::value << endl;
+  cout << "test<true, true>():" << test<true, true>().value << endl;
+  cout << "test<false, true>():" << test<false, true>().value << endl;
   return 0;
 }

12e4bc138392a4ba08aee85f10326dbb25103b3e
yixinglu <yxlyee@gmail.com>
2016-07-13 23:28:07 +0800

    add get first if test template

---
+++ mpl/get_first_if.h
@@ -0,0 +1,25 @@
+#include <type_traits>
+
+namespace mpl {
+
+template <template <typename T> class Predicate, typename Default, typename... Rest>
+struct get_first_if_impl;
+
+template <template <typename T> class Predicate, typename Default, typename T, typename... Rest>
+struct get_first_if_impl<Predicate, Default, T, Rest...> {
+  using type = typename std::conditional<
+                   Predicate<T>::value,
+                   T,
+                   typename get_first_if_impl<Predicate, Default, Rest...>::type
+               >::type;
+};
+
+template <template <typename T> class Predicate, typename Default>
+struct get_first_if_impl<Predicate, Default> {
+  using type = Default;
+};
+
+template <template <typename T> class Predicate, typename Default, typename...T>
+using get_first_if = typename get_first_if_impl<Predicate, Default, T...>::type;
+
+}
--- mpl/main.cc
+++ mpl/main.cc
@@ -1,11 +1,29 @@
 #include "index_sequence.h"
+#include "get_first_if.h"
 
 using namespace mpl;
+
+template <typename T>
+struct is_inconsistent : std::integral_constant<bool, !T::value> {};
+
+struct consistent {
+  static constexpr bool value = true;
+};
+
+struct inconsistent {
+  static constexpr bool value = false;
+};
 
 int main() {
 
   using index_sequence2 = make_sequence_t<2>;
   static_assert(std::is_same<index_sequence2, index_sequence<0, 1>>::value, "");
+
+  using first_t = get_first_if<is_inconsistent, consistent, consistent, inconsistent, consistent>;
+  static_assert(std::is_same<inconsistent, first_t>::value, "");
+
+  using default_t = get_first_if<is_inconsistent, consistent>;
+  static_assert(std::is_same<consistent, default_t>::value, "");
 
   return 0;
 }

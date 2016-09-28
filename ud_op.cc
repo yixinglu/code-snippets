73140ee41952a9dcbadea634054a82db7d7aab14
yixinglu <yxlyee@gmail.com>
2016-09-28 11:21:40 +0800

    add operator overload test

---
+++ ud_op.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct Foo {
+  Foo &operator +(int i) {
+    cout << "operator +" << endl;
+    return *this;
+  }
+};
+
+Foo &operator +(int i, Foo &foo) {
+  return foo + i;
+}
+
+int main() {
+  Foo foo;
+  int i = 0;
+  i + foo + i;
+
+  return 0;
+}

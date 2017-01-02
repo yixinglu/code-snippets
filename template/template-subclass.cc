c5688740dd9837e9f4c93f48e1dd36d103dc2e3b
yixinglu <yxlyee@gmail.com>
2017-01-02 14:57:44 +0800

    template and virtual function

--- template/template-subclass.cc
+++ template/template-subclass.cc
@@ -5,8 +5,10 @@ template <typename Sub>
 struct Base {
   using sub_type = Sub;
 
+#if 0
   // ERROR: incomplete sub_type
-  // auto sub() -> decltype(sub_type{}) { return {sub_type{}}; }
+  auto sub() -> decltype(sub_type{}) { return {sub_type{}}; }
+#endif
 };
 
 struct Sub : Base<Sub> {};
@@ -19,10 +21,12 @@ struct Base : Sub::_base_t {
     std::cout << "base" << std::endl;
     return {};
   }
-  // auto base2() -> decltype(Base<Sub>{}) {
-  //   std::cout << "base2" << std::endl;
-  //   return {};
-  // }
+#if 0
+  auto base2() -> decltype(Base<Sub>{}) {
+    std::cout << "base2" << std::endl;
+    return {};
+  }
+#endif
 };
 
 struct Sub {
@@ -35,14 +39,33 @@ struct Sub {
 };
 }  // namespace test2
 
+namespace test3 {
+#if 0
+struct Base {
+  template <typename T>
+  virtual Base& fun(T t) {
+    return *this;
+  }
+};
+
+struct Sub : Base {
+  template <typename T>
+  Sub& fun(T t) override {
+    Base::fun<T>(t);
+    return *this;
+  }
+}
+#endif
+}  // namespace test3
+
 int main() {
   // test1::Sub().sub();
   test2::Base<test2::Sub> sub2;
-  sub2
-    .sub()
-    .base()
-    // .base2()
-    ;
+  // sub2.sub().base().base2();
+  sub2.sub().base();
+
+  // test3::Sub sub3;
+  // sub3.fun(5);
 
   return 0;
 }

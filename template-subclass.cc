3d11f36ee959bdb43dc79abdec6d95e151d0562f
yixinglu <yxlyee@gmail.com>
2017-01-02 12:58:56 +0800

    update template test

--- template-subclass.cc
+++ template-subclass.cc
@@ -1,16 +1,40 @@
+#include <iostream>
 
+namespace test1 {
 template <typename Sub>
 struct Base {
   using sub_type = Sub;
 
   // ERROR: incomplete sub_type
-  auto sub() -> decltype(sub_type{}) { return {sub_type{}}; }
+  // auto sub() -> decltype(sub_type{}) { return {sub_type{}}; }
 };
 
 struct Sub : Base<Sub> {};
+}  // namespace test1
+
+namespace test2 {
+template <typename Sub>
+struct Base : Sub::_base_t {
+  auto base() -> Base<Sub> {
+    std::cout << "base" << std::endl;
+    return {};
+  }
+};
+
+struct Sub {
+  struct _base_t {
+    auto sub() -> Base<Sub> {
+      std::cout << "sub" << std::endl;
+      return {};
+    }
+  };
+};
+}  // namespace test2
 
 int main() {
-  Sub().sub();
+  // test1::Sub().sub();
+  test2::Base<test2::Sub> sub2;
+  sub2.sub().base();
 
   return 0;
 }

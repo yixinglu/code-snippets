5e3a2cc9474cbd123c3f49e9c89222f527139ea3
yixinglu <yxlyee@gmail.com>
2017-01-03 11:49:52 +0800

    add pip install genpac

--- bash/generate-pac.sh
+++ bash/generate-pac.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ! type "genpac" > /dev/null; then
+		pip install --upgrade pip
+		pip install genpac
+fi
+
 genpac --proxy="SOCKS5 127.0.0.1:1080" \
 		--gfwlist-proxy="SOCKS5 127.0.0.1:1080" \
 		-o autoproxy.pac \

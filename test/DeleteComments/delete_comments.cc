c2d4932e21d85cf72f01f79e2976aa43a68c1119
yixinglu <yxlyee@gmail.com>
2017-12-18 23:32:24 +0800

    delete duplicate string

--- test/DeleteComments/delete_comments.cc
+++ test/DeleteComments/delete_comments.cc
@@ -55,99 +55,98 @@ enum class Status { kDrop, kPrint };
 struct StateMachine {
   State state;
   Status status;
-  std::string ch;
 };
 
 StateMachine state_machine[(int)State::kCount][(int)Event::kCount] = {
     {
         // kCode
-        {State::kCommentStart, Status::kDrop, ""},   // kSlash
-        {State::kCode, Status::kPrint, ""},          // kEscape
-        {State::kCode, Status::kPrint, ""},          // kStar
-        {State::kCode, Status::kPrint, ""},          // kNewLine
-        {State::kStringInside, Status::kPrint, ""},  // kDoubleQuotes
-        {State::kCharInside, Status::kPrint, ""},    // kQuote
-        {State::kCode, Status::kPrint, ""}           // kOtherwise
+        {State::kCommentStart, Status::kDrop},   // kSlash
+        {State::kCode, Status::kPrint},          // kEscape
+        {State::kCode, Status::kPrint},          // kStar
+        {State::kCode, Status::kPrint},          // kNewLine
+        {State::kStringInside, Status::kPrint},  // kDoubleQuotes
+        {State::kCharInside, Status::kPrint},    // kQuote
+        {State::kCode, Status::kPrint}           // kOtherwise
     },
     {
         // kCommentStart
-        {State::kCommentLine, Status::kDrop, ""},            // kSlash
-        {State::kCode, Status::kPrint, "/"},                 // kEscape
-        {State::kCommentMultilineStart, Status::kDrop, ""},  // kStar
-        {State::kCode, Status::kPrint, "/"},                 // kNewLine
-        {State::kCode, Status::kPrint, "/"},                 // kDoubleQuotes
-        {State::kCode, Status::kPrint, "/"},                 // kQuote
-        {State::kCode, Status::kPrint, "/"}                  // kOtherwise
+        {State::kCommentLine, Status::kDrop},            // kSlash
+        {State::kCode, Status::kPrint},                  // kEscape
+        {State::kCommentMultilineStart, Status::kDrop},  // kStar
+        {State::kCode, Status::kPrint},                  // kNewLine
+        {State::kCode, Status::kPrint},                  // kDoubleQuotes
+        {State::kCode, Status::kPrint},                  // kQuote
+        {State::kCode, Status::kPrint}                   // kOtherwise
     },
     {
         // kCommentLine
-        {State::kCommentLine, Status::kDrop, ""},  // kSlash
-        {State::kCommentLine, Status::kDrop, ""},  // kEscape
-        {State::kCommentLine, Status::kDrop, ""},  // kStar
-        {State::kCode, Status::kPrint, ""},        // kNewLine
-        {State::kCommentLine, Status::kDrop, ""},  // kDoubleQuotes
-        {State::kCommentLine, Status::kDrop, ""},  // kQuote
-        {State::kCommentLine, Status::kDrop, ""}   // kOtherwise
+        {State::kCommentLine, Status::kDrop},  // kSlash
+        {State::kCommentLine, Status::kDrop},  // kEscape
+        {State::kCommentLine, Status::kDrop},  // kStar
+        {State::kCode, Status::kPrint},        // kNewLine
+        {State::kCommentLine, Status::kDrop},  // kDoubleQuotes
+        {State::kCommentLine, Status::kDrop},  // kQuote
+        {State::kCommentLine, Status::kDrop}   // kOtherwise
     },
     {
         // kCommentMultilineStart
-        {State::kCommentMultilineStart, Status::kDrop, ""},   // kSlash
-        {State::kCommentMultilineStart, Status::kDrop, ""},   // kEscape
-        {State::kCommentMultilineHold, Status::kDrop, ""},    // kStar
-        {State::kCommentMultilineStart, Status::kPrint, ""},  // kNewLine
-        {State::kCommentMultilineStart, Status::kDrop, ""},   // kDoubleQuotes
-        {State::kCommentMultilineStart, Status::kDrop, ""},   // kQuote
-        {State::kCommentMultilineStart, Status::kDrop, ""}    // kOtherwise
+        {State::kCommentMultilineStart, Status::kDrop},   // kSlash
+        {State::kCommentMultilineStart, Status::kDrop},   // kEscape
+        {State::kCommentMultilineHold, Status::kDrop},    // kStar
+        {State::kCommentMultilineStart, Status::kPrint},  // kNewLine
+        {State::kCommentMultilineStart, Status::kDrop},   // kDoubleQuotes
+        {State::kCommentMultilineStart, Status::kDrop},   // kQuote
+        {State::kCommentMultilineStart, Status::kDrop}    // kOtherwise
     },
     {
         // kCommentMultilineHold
-        {State::kCode, Status::kDrop, ""},                    // kSlash
-        {State::kCommentMultilineStart, Status::kDrop, ""},   // kEscape
-        {State::kCommentMultilineHold, Status::kDrop, ""},    // kStar
-        {State::kCommentMultilineStart, Status::kPrint, ""},  // kNewLine
-        {State::kCommentMultilineStart, Status::kDrop, ""},   // kDoubleQuotes
-        {State::kCommentMultilineStart, Status::kDrop, ""},   // kQuote
-        {State::kCommentMultilineStart, Status::kDrop, ""}    // kOtherwise
+        {State::kCode, Status::kDrop},                    // kSlash
+        {State::kCommentMultilineStart, Status::kDrop},   // kEscape
+        {State::kCommentMultilineHold, Status::kDrop},    // kStar
+        {State::kCommentMultilineStart, Status::kPrint},  // kNewLine
+        {State::kCommentMultilineStart, Status::kDrop},   // kDoubleQuotes
+        {State::kCommentMultilineStart, Status::kDrop},   // kQuote
+        {State::kCommentMultilineStart, Status::kDrop}    // kOtherwise
     },
     {
         // kStringInside
-        {State::kStringInside, Status::kPrint, ""},  // kSlash
-        {State::kStringEscape, Status::kPrint, ""},  // kEscape
-        {State::kStringInside, Status::kPrint, ""},  // kStar
-        {State::kStringInside, Status::kPrint, ""},  // kNewLine
-        {State::kCode, Status::kPrint, ""},          // kDoubleQuotes
-        {State::kStringInside, Status::kPrint, ""},  // kQuote
-        {State::kStringInside, Status::kPrint, ""}   // kOtherwise
+        {State::kStringInside, Status::kPrint},  // kSlash
+        {State::kStringEscape, Status::kPrint},  // kEscape
+        {State::kStringInside, Status::kPrint},  // kStar
+        {State::kStringInside, Status::kPrint},  // kNewLine
+        {State::kCode, Status::kPrint},          // kDoubleQuotes
+        {State::kStringInside, Status::kPrint},  // kQuote
+        {State::kStringInside, Status::kPrint}   // kOtherwise
     },
     {
         // kStringEscape
-        {State::kStringInside, Status::kPrint, ""},  // kSlash
-        {State::kStringInside, Status::kPrint, ""},  // kEscape
-        {State::kStringInside, Status::kPrint, ""},  // kStar
-        {State::kStringInside, Status::kPrint, ""},  // kNewLine
-        {State::kStringInside, Status::kPrint, ""},  // kDoubleQuotes
-        {State::kStringInside, Status::kPrint, ""},  // kQuote
-        {State::kStringInside, Status::kPrint, ""}   // kOtherwise
+        {State::kStringInside, Status::kPrint},  // kSlash
+        {State::kStringInside, Status::kPrint},  // kEscape
+        {State::kStringInside, Status::kPrint},  // kStar
+        {State::kStringInside, Status::kPrint},  // kNewLine
+        {State::kStringInside, Status::kPrint},  // kDoubleQuotes
+        {State::kStringInside, Status::kPrint},  // kQuote
+        {State::kStringInside, Status::kPrint}   // kOtherwise
     },
     {
         // kCharInside
-        {State::kCharInside, Status::kPrint, ""},  // kSlash
-        {State::kCharEscape, Status::kPrint, ""},  // kEscape
-        {State::kCharInside, Status::kPrint, ""},  // kStar
-        {State::kCharInside, Status::kPrint, ""},  // kNewLine
-        {State::kCharInside, Status::kPrint, ""},  // kDoubleQuotes
-        {State::kCode, Status::kPrint, ""},        // kQuote
-        {State::kCharInside, Status::kPrint, ""}   // kOtherwise
+        {State::kCharInside, Status::kPrint},  // kSlash
+        {State::kCharEscape, Status::kPrint},  // kEscape
+        {State::kCharInside, Status::kPrint},  // kStar
+        {State::kCharInside, Status::kPrint},  // kNewLine
+        {State::kCharInside, Status::kPrint},  // kDoubleQuotes
+        {State::kCode, Status::kPrint},        // kQuote
+        {State::kCharInside, Status::kPrint}   // kOtherwise
     },
     {
         // kCharEscape
-        {State::kCharInside, Status::kPrint, ""},  // kSlash
-        {State::kCharInside, Status::kPrint, ""},  // kEscape
-        {State::kCharInside, Status::kPrint, ""},  // kStar
-        {State::kCharInside, Status::kPrint, ""},  // kNewLine
-        {State::kCharInside, Status::kPrint, ""},  // kDoubleQuotes
-        {State::kCharInside, Status::kPrint, ""},  // kQuote
-        {State::kCharInside, Status::kPrint, ""}   // kOtherwise
+        {State::kCharInside, Status::kPrint},  // kSlash
+        {State::kCharInside, Status::kPrint},  // kEscape
+        {State::kCharInside, Status::kPrint},  // kStar
+        {State::kCharInside, Status::kPrint},  // kNewLine
+        {State::kCharInside, Status::kPrint},  // kDoubleQuotes
+        {State::kCharInside, Status::kPrint},  // kQuote
+        {State::kCharInside, Status::kPrint}   // kOtherwise
     }};
 
 class FileReader {
@@ -197,7 +196,10 @@ int main() {
     int event = (int)freader.make_event();
     auto machine = state_machine[(int)state][event];
     if (machine.status == Status::kPrint) {
-      fout << machine.ch << freader.current_char();
+      if (state == State::kCommentStart) {
+        fout << '/';
+      }
+      fout << freader.current_char();
     }
     state = machine.state;
   }
